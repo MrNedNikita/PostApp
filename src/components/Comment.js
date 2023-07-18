55e5bcf2bd7d4270804f225bb5e78445946a1181
Mykyta Nedialkov <80514594+MrNedNick@users.noreply.github.com>
2023-07-18 11:30:16 +0300

    Edit comment component and PostScreen

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,73 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Button, Card, Text, TextInput } from 'react-native-paper';
 
-const Comment = () => {
+const Comment = ({ comment, onDelete, onSaveEdit }) => {
+  const [editing, setEditing] = useState(false);
+  const [text, setText] = useState(comment.text);
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleSave = () => {
+    if (!text) {
+      return alert('Please fill all fields!');
+    }
+    onSaveEdit(comment.id, text);
+    setEditing(false);
+  };
+
+  const handleCancel = () => {
+    setText(comment.text);
+    setEditing(false);
+  };
+
   return (
-    <View>
-      <Text>Comment</Text>
-    </View>
+    <Card style={styles.comment}>
+      {editing ? (
+        <View>
+          <TextInput
+            mode="outlined"
+            style={styles.input}
+            value={text}
+            onChangeText={setText}
+          />
+          <Card.Actions>
+            <Button onPress={handleCancel}>Cancel</Button>
+            <Button onPress={handleSave}>Save</Button>
+          </Card.Actions>
+        </View>
+      ) : (
+        <View>
+          <Card.Content>
+            <Text style={styles.text}>{comment.text}</Text>
+          </Card.Content>
+          <Card.Actions>
+            <Button onPress={handleEdit}>Edit</Button>
+            <Button onPress={() => onDelete(comment.id)}>Delete</Button>
+          </Card.Actions>
+        </View>
+      )}
+    </Card>
   );
 };
+
+const styles = StyleSheet.create({
+  comment: {
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 8,
+  },
+  text: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    borderBottomWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
 
 export default Comment;
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,15 +1,99 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Comment from '../components/Comment';
+import { Button, Text, TextInput, Card } from 'react-native-paper';
 
 const PostScreen = ({ route }) => {
   const { post } = route.params;
+  const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState('');
+
+  const handleAddComment = () => {
+    if (!commentText) {
+      return alert('Please fill all fields!');
+    }
+    const newComment = {
+      id: new Date().getTime(),
+      text: commentText,
+      postId: post.id,
+    };
+    setComments([newComment, ...comments]);
+    setCommentText('');
+  };
+
+  const handleDeleteComment = (id) => {
+    setComments(comments.filter((comment) => comment.id !== id));
+  };
+
+  const handleSaveEditComment = (id, text) => {
+    setComments(
+      comments.map((comment) => {
+        if (comment.id === id) {
+          return {
+            ...comment,
+            text: text,
+          };
+        }
+        return comment;
+      })
+    );
+  };
 
   return (
-    <View>
-      <Text>{ post.title }</Text>
-      <Text>{ post.body }</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{post.title}</Text>
+      <Text style={styles.body}>{post.body}</Text>
+      <Card style={styles.commentForm}>
+        <TextInput
+          mode="outlined"
+          style={styles.input}
+          placeholder="Enter your comment"
+          value={commentText}
+          onChangeText={(text) => setCommentText(text)}
+        />
+        <Button style={styles.addButton} onPress={handleAddComment}>
+          Add Comment
+        </Button>
+      </Card>
+      {comments.map((comment) => (
+        <Comment
+          key={comment.id}
+          comment={comment}
+          onDelete={handleDeleteComment}
+          onSaveEdit={handleSaveEditComment}
+        />
+      ))}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 8,
+  },
+  body: {
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  commentForm: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+  },
+  input: {
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 12,
+    paddingHorizontal: 8,
+  },
+});
 
 export default PostScreen;

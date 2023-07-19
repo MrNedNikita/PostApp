3667d7b8645cb45c7becdb075cf2dd641e2afc4d
Mykyta Nedialkov <80514594+MrNedNick@users.noreply.github.com>
2023-07-19 17:49:51 +0300

    edit axios to fetch

--- src/store/actions/commentActions.js
+++ src/store/actions/commentActions.js
@@ -1,19 +1,16 @@
-import axios from 'axios';
-
 export const FETCH_COMMENTS = 'FETCH_COMMENTS';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 
-// const API_BASE_URL = 'https://my-json-server.typicode.com/MrNedNikita/PostApp/blob/main';
-// const API_BASE_URL = 'https://my_json_server.typicode.com/MrNedNikita/fakeServer';
 const API_BASE_URL = 'http://localhost:3000';
 
 export const fetchComments = () => {
   return async (dispatch) => {
     try {
-      const response = await axios.get(`${API_BASE_URL}/comments`);
-      dispatch({ type: FETCH_COMMENTS, payload: response.data });
+      const response = await fetch(`${API_BASE_URL}/comments`);
+      const data = await response.json();
+      dispatch({ type: FETCH_COMMENTS, payload: data });
     } catch (error) {
       console.error('Error fetching comments:', error);
     }
@@ -23,8 +20,15 @@ export const fetchComments = () => {
 export const addComment = (id, text, postId) => {
   return async (dispatch) => {
     try {
-      const response = await axios.post(`${API_BASE_URL}/comments`, { id, text, postId });
-      dispatch({ type: ADD_COMMENT, payload: response.data });
+      const response = await fetch(`${API_BASE_URL}/comments`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ id, text, postId }),
+      });
+      const data = await response.json();
+      dispatch({ type: ADD_COMMENT, payload: data });
     } catch (error) {
       console.error('Error adding comment:', error);
     }
@@ -32,11 +36,27 @@ export const addComment = (id, text, postId) => {
 };
 
 export const deleteComment = (id) => async (dispatch) => {
-  await axios.delete(`${API_BASE_URL}/comments/${id}`);
-  dispatch({ type: DELETE_COMMENT, payload: id });
+  try {
+    await fetch(`${API_BASE_URL}/comments/${id}`, {
+      method: 'DELETE',
+    });
+    dispatch({ type: DELETE_COMMENT, payload: id });
+  } catch (error) {
+    console.error('Error deleting comment:', error);
+  }
 };
 
 export const editComment = (id, text, postId) => async (dispatch) => {
-  await axios.put(`${API_BASE_URL}/comments/${id}`, { text, postId });
-  dispatch({ type: EDIT_COMMENT, payload: { id, text, postId } });
+  try {
+    await fetch(`${API_BASE_URL}/comments/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text, postId }),
+    });
+    dispatch({ type: EDIT_COMMENT, payload: { id, text, postId } });
+  } catch (error) {
+    console.error('Error editing comment:', error);
+  }
 };
--- src/store/actions/postActions.js
+++ src/store/actions/postActions.js
@@ -1,30 +1,59 @@
-import axios from 'axios';
-
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const ADD_POST = 'ADD_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const EDIT_POST = 'EDIT_POST';
 
-// const API_BASE_URL = 'https://my-json-server.typicode.com/MrNedNikita/PostApp/blob/main';
-// const API_BASE_URL = 'https://my-json-server.typicode.com/MrNedNikita/fakeServer';
 const API_BASE_URL = 'http://localhost:3000';
 
 export const fetchPosts = () => async (dispatch) => {
-  const response = await axios.get(`${API_BASE_URL}/posts`);
-  dispatch({ type: FETCH_POSTS, payload: response.data });
+  try {
+    const response = await fetch(`${API_BASE_URL}/posts`);
+    const data = await response.json();
+    dispatch({ type: FETCH_POSTS, payload: data });
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
 };
 
 export const addPost = (id, title, body) => async (dispatch) => {
-  const response = await axios.post(`${API_BASE_URL}/posts`, { id, title, body });
-  dispatch({ type: ADD_POST, payload: response.data });
+  try {
+    const response = await fetch(`${API_BASE_URL}/posts`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id, title, body }),
+    });
+    const data = await response.json();
+    dispatch({ type: ADD_POST, payload: data });
+  } catch (error) {
+    console.error('Error adding post:', error);
+  }
 };
 
 export const deletePost = (id) => async (dispatch) => {
-  await axios.delete(`${API_BASE_URL}/posts/${id}`);
-  dispatch({ type: DELETE_POST, payload: id });
+  try {
+    await fetch(`${API_BASE_URL}/posts/${id}`, {
+      method: 'DELETE',
+    });
+    dispatch({ type: DELETE_POST, payload: id });
+  } catch (error) {
+    console.error('Error deleting post:', error);
+  }
 };
 
 export const editPost = (id, title, body) => async (dispatch) => {
-  await axios.put(`${API_BASE_URL}/posts/${id}`, { title, body });
-  dispatch({ type: EDIT_POST, payload: { id, title, body } });
+  try {
+    await fetch(`${API_BASE_URL}/posts/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, body }),
+    });
+    dispatch({ type: EDIT_POST, payload: { id, title, body } });
+  } catch (error) {
+    console.error('Error editing post:', error);
+  }
 };
+

da3c7adf3712d133b268ca95ac69b5406ac20b9f
Mykyta Nedialkov <80514594+MrNedNick@users.noreply.github.com>
2023-07-19 15:38:08 +0300

    edit api url

--- src/store/actions/commentActions.js
+++ src/store/actions/commentActions.js
@@ -5,8 +5,8 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const DELETE_COMMENT = 'DELETE_COMMENT';
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 
-// const API_BASE_URL = 'https://my-json-server.typicode.com/MrNedNikita/PostApp/blob/main';
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://my-json-server.typicode.com/MrNedNikita/fakeServer';
+// const API_BASE_URL = 'http://localhost:3000';
 
 export const fetchComments = () => {
   return async (dispatch) => {
